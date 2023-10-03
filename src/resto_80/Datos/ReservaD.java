3f811427e5502256a4640605f2a606c089bf3aed
NicoPecina <nicolaspeci@gmail.com>
2023-10-03 17:56:16 -0300

    MeserosData

--- src/resto_80/Datos/ReservaD.java
+++ src/resto_80/Datos/ReservaD.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package resto_80.Datos;
 
 import java.sql.Connection;
@@ -17,10 +13,7 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import resto_80.Entidades.Reserva;
 
-/**
- *
- * @author Jesica
- */
+
 public class ReservaD {
     private Connection con = null;
     
--- src/resto_80/Resto_80.java
+++ src/resto_80/Resto_80.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package resto_80;
 
 import java.sql.Connection;
@@ -10,28 +5,26 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
 import resto_80.Datos.Conexión;
+import resto_80.Datos.MeseroD;
 import resto_80.Datos.ReservaD;
 import resto_80.Entidades.Mesa;
+import resto_80.Entidades.Mesero;
 import resto_80.Entidades.Reserva;
 
-/**
- *
- * @author Jesica
- */
 public class Resto_80 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Connection conex = Conexión.getConnection();
-        
-        Reserva r = new Reserva(1, "Perez Carlos", 36355207, LocalDate.now(), LocalTime.now(), true);
-        ReservaD rd = new ReservaD();
-        
-//        rd.agregarReserva(r);
-//        rd.cancelarReserva(1);
-        
+
+        MeseroD md = new MeseroD();
+
+//    Mesero m= new Mesero("Nicolas","Peciña","nicopeci",1234,true);
+//    md.agregarMesero(m);
+//    Mesero m= new Mesero (4,"Nicolas","Peciña","nicopeci",987654,true);
+//    md.modificarMesero(m);
+//    md.eliminarMesero(4);
+//    System.out.println(md.buscarMesero(4));
+//    System.out.println(md.listarMesero());
     }
-    
+
 }
