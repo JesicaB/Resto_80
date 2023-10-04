6a38e56aeb4598d0a34f5d597952dd699d719e55
JesicaB <100101914+JesicaB@users.noreply.github.com>
2023-10-04 17:12:33 -0300

    Update ProductoD.java

--- src/resto_80/Datos/ProductoD.java
+++ src/resto_80/Datos/ProductoD.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package resto_80.Datos;
 
 import java.sql.Connection;
@@ -18,147 +13,174 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import resto_80.Entidades.Producto;
+
 /**
  *
  * @author MANUEL
  */
 public class ProductoD {
+
     private Connection con = null;
-    
-    public ProductoD(){
+
+    public ProductoD() {
         con = Conexión.getConnection();
     }
-    
-    public void agregarProducto(Producto producto){
-        
-        String sql= "INSERT INTO productos (Nombre, Cantidad, Precio) "
-                + "VALUES (,?,?,?,?)";
-        
+
+    public void agregarProducto(Producto producto) {
+
+        String sql = "INSERT INTO productos (Nombre, Precio, Estado) "
+                + "VALUES (?,?,?)";
+
         try {
-            
-            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-            
+
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
             ps.setString(1, producto.getNombre());
-            ps.setInt(2, producto.getCantidad());
-            ps.setDouble(3, producto.getPrecio());
-            
+            ps.setDouble(2, producto.getPrecio());
+            ps.setBoolean(3, producto.isEstado());
+
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
-            
-            if(rs.next()){
+
+            if (rs.next()) {
                 producto.setIdProducto(rs.getInt(1));
                 JOptionPane.showMessageDialog(null, "Producto añadido con éxito.");
             }
-            
+
             ps.close();
-            
+
         } catch (SQLException ex) {
-           JOptionPane.showMessageDialog(null, "Hubo un error al cargar el Producto."
-                   + "Verifique que el producto no haya sido cargado anteriormente");
+            JOptionPane.showMessageDialog(null, "Hubo un error al cargar el Producto."
+                    + "Verifique que el producto no haya sido cargado anteriormente");
         }
     }
-    
-    public void modificarProducto(Producto producto){
-        String sql = "UPDATE productos SET Nombre=?,Cantidad=?,Precio=? WHERE idProducto=?";
-        
+
+    public void modificarProducto(Producto producto) {
+        String sql = "UPDATE productos SET Nombre=?,Precio=?,Estado=? WHERE idProducto=?";
+
         try {
-            
+
             PreparedStatement ps = con.prepareStatement(sql);
-            
+
             ps.setString(1, producto.getNombre());
-            ps.setInt(2, producto.getCantidad());
-            ps.setDouble(3, producto.getPrecio());
-                        
+            ps.setDouble(2, producto.getPrecio());
+            ps.setBoolean(3, producto.isEstado());
+            ps.setInt(4, producto.getIdProducto());
+
             int exito = ps.executeUpdate();
-            
-            if(exito == 1){
+
+            if (exito == 1) {
                 JOptionPane.showMessageDialog(null, "Producto modificado.");
             }
-            
+
             ps.close();
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla productos.");
         }
-       
-        
+
     }
-    
-    public void eliminarProducto(int idProducto){
+
+    public void eliminarProducto(int idProducto) {
         String sql = "DELETE FROM productos WHERE idProducto=?";
-        
+
         try {
-            
+
             PreparedStatement ps = con.prepareStatement(sql);
-            
+
             ps.setInt(1, idProducto);
-            
+
+            int bp = ps.executeUpdate();
+
+            if (bp == 1) {
+                JOptionPane.showMessageDialog(null, "Producto Eliminado");
+            }
+            ps.close();
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Hubo un error y no se pudo eliminar el producto seleccionado");
         }
-        
+
     }
-    
-    public List<Producto> listarProductos()
-     {
-         String sql= "SELECT * FROM productos";
-         ArrayList<Producto> productos= new ArrayList<>();
-         
+
+    public void bajaProducto(int idProducto) {
+        String sql = "UPDATE productos SET Estado=0 WHERE idProducto=?";
+
         try {
             PreparedStatement ps = con.prepareStatement(sql);
-            
-            ResultSet rs= ps.executeQuery();
-            
-            while (rs.next()){
-                
-                Producto producto =new Producto();
+            ps.setInt(1, idProducto);
+
+            int bp = ps.executeUpdate();
+
+            if (bp == 1) {
+                JOptionPane.showMessageDialog(null, "Producto dado de Baja");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Hubo un error y no se pudo Dar de Baja el producto seleccionado");
+        }
+
+    }
+
+    public List<Producto> listarProductos() {
+        String sql = "SELECT * FROM productos";
+        ArrayList<Producto> productos = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                Producto producto = new Producto();
                 producto.setIdProducto(rs.getInt("idProducto"));
                 producto.setNombre(rs.getString("Nombre"));
-                producto.setCantidad(rs.getInt("Cantidad"));
                 producto.setPrecio(rs.getDouble("Precio"));
-                                
+                producto.setEstado(rs.getBoolean("Estado"));
+
                 productos.add(producto);
             }
-            
-             ps.close();
-             
+
+            ps.close();
+
         } catch (SQLException ex) {
-           JOptionPane.showMessageDialog(null, "Error al acceder a la tabla productos.");
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla productos.");
         }
-        
+
         return productos;
-    
+
     }
-    
-    public Producto buscarProducto(int id)
-     {
-         String sql= "SELECT * FROM productos WHERE idProducto = ?";
-         Producto producto=null;
+
+    public Producto buscarProducto(int id) {
+        String sql = "SELECT * FROM productos WHERE idProducto = ?";
+        Producto producto = null;
         try {
             PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1,id);
-            ResultSet rs= ps.executeQuery();
-            
-            if (rs.next()){
-                
-                producto=new Producto();
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                producto = new Producto();
                 producto.setIdProducto(rs.getInt("idProducto"));
                 producto.setNombre(rs.getString("Nombre"));
-                producto.setCantidad(rs.getInt("Cantidad"));
                 producto.setPrecio(rs.getDouble("Precio"));
-                
-            }else{
+                producto.setEstado(rs.getBoolean("Estado"));
+
+            } else {
                 JOptionPane.showMessageDialog(null, "El producto no existe o fue eliminado");
             }
-            
-             ps.close();
-             
+
+            ps.close();
+
         } catch (SQLException ex) {
-           JOptionPane.showMessageDialog(null, "Error al acceder a la tabla producto.");
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla producto.");
         }
-        
+
         return producto;
-         
-     }
+
+    }
 }
