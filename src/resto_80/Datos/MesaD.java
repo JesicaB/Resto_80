807d72879c76b8403de48c615ce4e609b2350c5c
Paikan75 <pabloarielastete@gmail.com>
2023-10-05 15:21:37 -0300

    mesaD

---
+++ src/resto_80/Datos/MesaD.java
@@ -0,0 +1,177 @@
+package resto_80.Datos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import resto_80.Entidades.Mesa;
+
+
+
+public class MesaD {
+ private Connection con = null;
+
+    public MesaD() {
+        con = Conexión.getConnection();   
+    
+    }
+     public void agregarMesa(Mesa mesa) {
+
+        String sql = "INSERT INTO mesa (Numero, Estado) "
+                + "VALUES (?,?)";
+
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1, mesa.getNumeroMesa());
+            ps.setBoolean(2, mesa.isEstado());
+           
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                mesa.setIdMesa(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Mesa Creada con Exito");
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Hubo un error al Inicializar la Mesa."
+                    + "Verifique que la Mesa no haya sido cargado anteriormente");
+        }
+    }
+
+    public void modificarMesa(Mesa mesa) {
+        String sql = "UPDATE mesa SET Numero=?, Estado=? WHERE idMesa=?";
+
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setInt(1, mesa.getNumeroMesa());
+            ps.setBoolean(2, mesa.isEstado());
+           
+
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Producto modificado.");
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla productos.");
+        }
+
+    }
+
+    public void eliminarMesa(int idMesa) {
+        String sql = "DELETE FROM productos WHERE idProducto=?";
+
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setInt(1, idMesa);
+
+            int bm = ps.executeUpdate();
+
+            if (bm == 1) {
+                JOptionPane.showMessageDialog(null, "Mesa Eliminada");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Hubo un error y no se pudo eliminar la Mesa seleccionada");
+        }
+
+    }
+
+    public void bajaMesa(int idMesa) {
+        String sql = "UPDATE mesa SET Estado=0 WHERE idMesa=?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idMesa);
+
+            int bm = ps.executeUpdate();
+
+            if (bm == 1) {
+                JOptionPane.showMessageDialog(null, "Mesa dada de Baja");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Hubo un error y no se pudo Dar de Baja la Mesa seleccionada");
+        }
+
+    }
+
+    public List<Mesa> listarMesas() {
+        String sql = "SELECT * FROM mesa";
+        ArrayList<Mesa> mesas = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                Mesa mesa = new Mesa();
+                mesa.setIdMesa(rs.getInt("idMesa"));
+                mesa.setNumeroMesa(rs.getInt("Numero"));
+                mesa.setEstado(rs.getBoolean("Estado"));
+
+               mesas.add(mesa);
+               
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Mesa.");
+        }
+
+        return mesas;
+
+    }
+
+    public Mesa buscarMesa(int id) {
+        String sql = "SELECT * FROM mesa WHERE idMesa = ?";
+        Mesa mesa = null;
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                mesa = new Mesa();
+                mesa.setIdMesa(rs.getInt("idMesa"));
+                mesa.setNumeroMesa(rs.getInt("Numero"));
+                mesa.setEstado(rs.getBoolean("Estado"));
+
+            } else {
+                JOptionPane.showMessageDialog(null, "El producto no existe o fue eliminado");
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla producto.");
+        }
+
+        return mesa;
+
+}
+}
+
